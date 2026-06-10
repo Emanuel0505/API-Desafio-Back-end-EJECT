935d541d93389d951b7718264962cecf168b9dda
Emanuel João <163889723+Emanuel0505@users.noreply.github.com>
2026-06-10 14:58:32 -0300

    feat(permissions): permissão de admin e lojista
    Foi adicionado a a permissão apenas para quem for autor de objeto pode modifica-lo, dando a permissão também para admins.

--- ecommerce/permissions.py
+++ ecommerce/permissions.py
@@ -12,5 +12,14 @@ class IsLojista(BasePermission):
         return bool(
             request.user and
             request.user.is_authenticated and
-            request.user.usertype == 'L'
+            (request.user.usertype == 'L' or request.user.is_staff)
         )
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        
+        
+        return bool(
+            request.user.is_staff or 
+            (hasattr(obj, 'author') and obj.author == request.user)
+        ) 
